03b797ce8b3630c00052aaba95f6db2e18396b06
BubbleFish <daforsastudia@gmail.com>
2025-07-08 20:14:38 +0300

    feat: Restaurant inherit NamedEntity and add default constructor

--- src/main/java/me/bbfh/graduation/restaurant/model/Restaurant.java
+++ src/main/java/me/bbfh/graduation/restaurant/model/Restaurant.java
@@ -1,26 +1,25 @@
 package me.bbfh.graduation.restaurant.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import me.bbfh.graduation.common.model.BaseEntity;
-import me.bbfh.graduation.common.validation.NoHtml;
+import me.bbfh.graduation.common.HasId;
+import me.bbfh.graduation.common.model.NamedEntity;
 
 @Entity
 @Table(name = "restaurant")
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Restaurant extends BaseEntity {
-    @Column(name = "name", nullable = false, unique = true)
-    @NotBlank
-    @Size(max = 64)
-    @NoHtml   // https://stackoverflow.com/questions/17480809
-    private String name;
+public class Restaurant extends NamedEntity implements HasId {
+    public Restaurant(Restaurant r) {
+        this(r.id, r.name);
+    }
+
+    public Restaurant(Integer id, String name) {
+        super(id, name);
+    }
 }
