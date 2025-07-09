49d72ee90609db7deb000e33fa2932b7b89bb992
BubbleFish <daforsastudia@gmail.com>
2025-07-09 14:07:45 +0300

    chore: Optimize imports

--- src/main/java/me/bbfh/graduation/restaurant/RestaurantUtil.java
+++ src/main/java/me/bbfh/graduation/restaurant/RestaurantUtil.java
@@ -3,7 +3,6 @@ package me.bbfh.graduation.restaurant;
 import lombok.experimental.UtilityClass;
 import me.bbfh.graduation.restaurant.model.Restaurant;
 import me.bbfh.graduation.restaurant.to.RestaurantTo;
-import me.bbfh.graduation.user.to.UserTo;
 
 @UtilityClass
 public class RestaurantUtil {
