51096d4f75a09a08d2a66375fce1b363db863eed
BubbleFish <daforsastudia@gmail.com>
2025-07-10 13:23:17 +0300

    test: Add tests for getAll(), get()

--- src/test/java/me/bbfh/graduation/restaurant/RestaurantTestData.java
+++ src/test/java/me/bbfh/graduation/restaurant/RestaurantTestData.java
@@ -2,8 +2,24 @@ package me.bbfh.graduation.restaurant;
 
 import me.bbfh.graduation.MatcherFactory;
 import me.bbfh.graduation.restaurant.model.Restaurant;
-import me.bbfh.graduation.user.model.User;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RestaurantTestData {
     public static final MatcherFactory.Matcher<Restaurant> RESTAURANT_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Restaurant.class);
+
+    private static final int RESTAURANT_ID_1 = 1;
+    private static final int RESTAURANT_ID_2 = 2;
+    private static final String RESTAURANT_NAME_1 = "Example Restaurant #1";
+    private static final String RESTAURANT_NAME_2 = "Example Restaurant #2";
+
+    public static List<Restaurant> RESTAURANTS = List.of(
+            new Restaurant(RESTAURANT_ID_1, RESTAURANT_NAME_1),
+            new Restaurant(RESTAURANT_ID_2, RESTAURANT_NAME_2)
+    );
+
+    public static final int RESTAURANT_ID_UNKNOWN = 1000;
+    public static final String RESTAURANT_NAME_3 = "Example Restaurant #3";
 }
--- src/test/java/me/bbfh/graduation/restaurant/web/RestaurantControllerTest.java
+++ src/test/java/me/bbfh/graduation/restaurant/web/RestaurantControllerTest.java
@@ -6,6 +6,7 @@ import me.bbfh.graduation.restaurant.RestaurantUtil;
 import me.bbfh.graduation.restaurant.model.Restaurant;
 import me.bbfh.graduation.restaurant.repository.RestaurantRepository;
 import me.bbfh.graduation.restaurant.to.RestaurantTo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,7 +14,11 @@ import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static me.bbfh.graduation.restaurant.RestaurantTestData.RESTAURANT_MATCHER;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static me.bbfh.graduation.restaurant.RestaurantTestData.*;
 import static me.bbfh.graduation.restaurant.web.RestaurantController.REST_URL;
 import static me.bbfh.graduation.user.UserTestData.ADMIN_MAIL;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -25,11 +30,28 @@ public class RestaurantControllerTest extends AbstractControllerTest {
     @Autowired
     private RestaurantRepository repository;
 
+    @BeforeEach
+    public void populate() {
+        repository.deleteAll();
+        RESTAURANTS = repository.saveAll(RESTAURANTS);
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void get() throws Exception {
+        ResultActions action = perform(MockMvcRequestBuilders.get(REST_URL_SLASH + RESTAURANTS.getFirst().id()))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        Restaurant created = RESTAURANT_MATCHER.readFromJson(action);
+        RESTAURANT_MATCHER.assertMatch(created, RESTAURANTS.getFirst());
+        RESTAURANT_MATCHER.assertMatch(repository.getExisted(created.id()), RESTAURANTS.getFirst());
+    }
+
     @Test
     @WithUserDetails(value = ADMIN_MAIL)
     void getNotFound() throws Exception {
-        perform(MockMvcRequestBuilders.get(REST_URL_SLASH + 69)
-                .param("restaurantId", "2"))
+        perform(MockMvcRequestBuilders.get(REST_URL_SLASH + RESTAURANT_ID_UNKNOWN))
                 .andDo(print())
                 .andExpect(status().isNotFound())
                 .andReturn();
@@ -37,8 +59,30 @@ public class RestaurantControllerTest extends AbstractControllerTest {
 
     @Test
     @WithUserDetails(value = ADMIN_MAIL)
+    void getAll() throws Exception {
+        ResultActions action = perform(MockMvcRequestBuilders.get(REST_URL))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        List<Restaurant> createdList = RESTAURANT_MATCHER.readListFromJson(action);
+
+        Map<Integer, Restaurant> expectedMap = RESTAURANTS.stream()
+                .collect(Collectors.toMap(Restaurant::getId, r -> r));
+
+        for (Restaurant created : createdList) {
+            Restaurant expected = expectedMap.get(created.getId());
+            if (expected == null) {
+                throw new IllegalStateException("There must be no extra restaurants");
+            }
+            RESTAURANT_MATCHER.assertMatch(created, expected);
+            RESTAURANT_MATCHER.assertMatch(repository.getExisted(created.id()), expected);
+        }
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
     void create() throws Exception {
-        RestaurantTo newTo = new RestaurantTo(null, "example restaurant");
+        RestaurantTo newTo = new RestaurantTo(null, RESTAURANT_NAME_3);
         Restaurant newRestaurant = RestaurantUtil.createNewFromTo(newTo);
         ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
                 .contentType(MediaType.APPLICATION_JSON)
