eea91f36ddbde0e66da30e5b4decb23faa27d100
BubbleFish <daforsastudia@gmail.com>
2025-08-27 17:18:14 +0300

    refactor: Optimize imports

--- src/main/java/me/bbfh/graduation/restaurant/repository/DishRepository.java
+++ src/main/java/me/bbfh/graduation/restaurant/repository/DishRepository.java
@@ -2,7 +2,6 @@ package me.bbfh.graduation.restaurant.repository;
 
 import me.bbfh.graduation.common.BaseRepository;
 import me.bbfh.graduation.restaurant.model.Dish;
-import me.bbfh.graduation.restaurant.model.Menu;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
