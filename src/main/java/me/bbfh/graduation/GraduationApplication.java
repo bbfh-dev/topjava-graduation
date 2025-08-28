91b8db554a57cb1410facde7678483ecac9e50b9
BubbleFish <daforsastudia@gmail.com>
2025-08-28 11:35:56 +0300

    refactor: Use DateTimeUtil.getCurrentDate()

--- src/main/java/me/bbfh/graduation/GraduationApplication.java
+++ src/main/java/me/bbfh/graduation/GraduationApplication.java
@@ -1,5 +1,6 @@
 package me.bbfh.graduation;
 
+import me.bbfh.graduation.common.util.DateTimeUtil;
 import me.bbfh.graduation.restaurant.model.Dish;
 import me.bbfh.graduation.restaurant.model.Menu;
 import me.bbfh.graduation.restaurant.model.Restaurant;
@@ -10,8 +11,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class GraduationApplication {
@@ -36,9 +35,9 @@ public class GraduationApplication {
         Restaurant restaurantA = restaurantRepository.save(new Restaurant(null, "My Restaurant A"));
         Restaurant restaurantB = restaurantRepository.save(new Restaurant(null, "My Restaurant B"));
 
-        Menu menuA1 = menuRepository.save(new Menu(null, LocalDate.now().minusDays(1), restaurantA));
-        Menu menuA2 = menuRepository.save(new Menu(null, LocalDate.now(), restaurantA));
-        menuRepository.save(new Menu(null, LocalDate.now(), restaurantB));
+        Menu menuA1 = menuRepository.save(new Menu(null, DateTimeUtil.getCurrentDate().minusDays(1), restaurantA));
+        Menu menuA2 = menuRepository.save(new Menu(null, DateTimeUtil.getCurrentDate(), restaurantA));
+        menuRepository.save(new Menu(null, DateTimeUtil.getCurrentDate(), restaurantB));
 
         dishRepository.save(new Dish(null, "My Dish A1", (long) 1700, menuA2));
         dishRepository.save(new Dish(null, "My Dish A2", (long) 2550, menuA2));
