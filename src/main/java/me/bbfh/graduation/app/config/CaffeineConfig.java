31d0e4efd708857c646549afdde6155fd45fe10e
BubbleFish <daforsastudia@gmail.com>
2025-09-12 16:01:44 +0300

    refactor: Remove cache configuration from application.yaml and change CaffeineConfig

--- src/main/java/me/bbfh/graduation/app/config/CaffeineConfig.java
+++ src/main/java/me/bbfh/graduation/app/config/CaffeineConfig.java
@@ -2,6 +2,7 @@ package me.bbfh.graduation.app.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,14 +13,17 @@ import java.util.concurrent.TimeUnit;
 public class CaffeineConfig {
 
     @Bean
-    public Caffeine caffeine() {
-        return Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES);
+    public CacheManager cacheManager() {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("users", "menus", "votes", "restaurants");
+        caffeineCacheManager.setCaffeine(getCaffeine());
+        return caffeineCacheManager;
     }
 
-    @Bean
-    public CacheManager cacheManager(Caffeine caffeine) {
-        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-        caffeineCacheManager.setCaffeine(caffeine);
-        return caffeineCacheManager;
+    public Caffeine<Object, Object> getCaffeine() {
+        return Caffeine.newBuilder()
+                // https://www.javadoc.io/doc/com.github.ben-manes.caffeine/caffeine/2.2.2/com/github/benmanes/caffeine/cache/Caffeine.html#maximumSize
+                // During testing this number seemed like the best middle ground.
+                .maximumSize(5_000)
+                .expireAfterWrite(60, TimeUnit.MINUTES);
     }
 }
